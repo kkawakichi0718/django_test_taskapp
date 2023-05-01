91bf0a3382736153432819f17c38c58f958df309
kkawakichi0718 <kkkawaki@icloud.com>
2023-05-01 18:30:07 +0900

    first commit

---
+++ TaskApp/task_project/taskapp/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import *
+from .forms import *
+
+def index(request):
+    # return HttpResponse('Hi')
+    tasks = Task.objects.all()
+    
+    form = TaskForm()
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    
+    context = {'tasks':tasks, 'form': form}
+    return render(request, 'taskapp/index.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    
+    form = TaskForm(instance=task)
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+    context = {'form': form}
+    
+    return render(request, 'taskapp/update_task.html', context)
+
+def deleteTask(request,pk):
+    item = Task.objects.get(id=pk)
+    
+    if request.method == 'POST':
+        print(item)
+        item.delete()
+        return redirect('/')
+    
+    context = {'item': item}
+    return render(request, 'taskapp/delete.html', context)
